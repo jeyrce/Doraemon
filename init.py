26e5902f58b012fec59005ffd361a9fbd60b543a
Jeeyshe <jeeyshe@gmail.com>
2020-09-20 00:52:30 +0800

    增加部署初始化脚本

--- init.py
+++ init.py
@@ -1,9 +1,27 @@
 # coding=utf-8
 """
-__purpose__ = ...
+__purpose__ = 项目部署时的初始化操作
 __author__  = JeeysheLu [Jeeyshe@gmail.com] [https://www.lujianxin.com/] [2020/9/19 21:00]
     
     Copyright (c) 2020 JeeysheLu
     
 This software is licensed to you under the MIT License. Looking forward to making it better.
 """
+import os
+
+import django
+
+from Doraemon.model import System
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Doraemon.settings")
+django.setup()
+
+KEYS = [
+    {"key": "SHOW_DUTY_DAYS", "value": "7", "remark": "首页显示的值班天数"},
+    {"key": "UPGRADING", "value": "0", "remark": "网站是否处于维护状态"},
+]
+
+if __name__ == '__main__':
+    # 初始化数据库表
+    for key in KEYS:
+        System.objects.create(**key)
