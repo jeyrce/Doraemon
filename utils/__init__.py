ce8c9d649f866ee601643eccefed7d3cd35fd6e2
Jeeyshe <jeeyshe@gmail.com>
2020-09-20 00:53:57 +0800

    增加工具类

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,31 @@
 # coding=utf-8
 """
-__purpose__ = ...
+__purpose__ = 常用工具类
 __author__  = JeeysheLu [Jeeyshe@gmail.com] [https://www.lujianxin.com/] [2020/9/19 17:51]
     
     Copyright (c) 2020 JeeysheLu
     
 This software is licensed to you under the MIT License. Looking forward to making it better.
 """
+import logging
+
+from Doraemon.model import System
+
+logger = logging.getLogger(__name__)
+
+
+def get_from_db(key, type_, default=None):
+    """
+    key: 获取的键
+    type_: 预期的值类型, 也可以是一个可执行对象
+    default: 获取不到值时的默认值
+    """
+    item = System.objects.filter(key=key).first()
+    if not item:
+        return default
+    assert callable(type_), f"Not supported type: {type_}"
+    try:
+        return type_(item)
+    except Exception as e:
+        logger.exception(e)
+    return default
