aeea1619a617d317a2a1c326a8db7325d4935126
Jeeyshe <jeeyshe@gmail.com>
2020-09-21 21:40:24 +0800

    增加查询方法

--- utils/__init__.py
+++ utils/__init__.py
@@ -29,3 +29,21 @@ def get_from_db(key, type_, default=None):
     except Exception as e:
         logger.exception(e)
     return default
+
+
+def get_next_username(username: str):
+    """
+    获取下一个值班人员用户名
+    """
+    duty_loop = (get_from_db("DUTY_LOOP", list, []))
+    loop_length = len(duty_loop)
+    if loop_length == 0:
+        logger.error(f"got an unexpected duty_loop: {duty_loop}")
+    username = username.strip()
+    if username in duty_loop:
+        next_index = duty_loop.index(username) + 1
+        if next_index >= loop_length:
+            return duty_loop[next_index - loop_length]
+        return duty_loop[next_index]
+    logger.error(f"username: {username} not in duty_loop: {duty_loop}")
+    return None
