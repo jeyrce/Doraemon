d123c313ca4db94aa152caebdec86b088c26e071
Jeeyshe <jeeyshe@gmail.com>
2020-09-21 22:05:17 +0800

    fix settings

--- Doraemon/settings.py
+++ Doraemon/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import pymysql
+pymysql.version_info = (1, 4, 13, "final", 0)
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
