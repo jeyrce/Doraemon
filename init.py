3ba5868f73b5f9293abb7c4a615f3d9c214ddaf0
Jeeyshe <jeeyshe@gmail.com>
2020-09-21 21:41:00 +0800

    部署初始化

--- init.py
+++ init.py
@@ -14,14 +14,31 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Doraemon.settings")
 django.setup()
 
-from Doraemon.model import System
-
-KEYS = [
-    {"key": "SHOW_DUTY_DAYS", "value": "7", "remark": "首页显示的值班天数"},
-    {"key": "UPGRADING", "value": "0", "remark": "网站是否处于维护状态"},
-]
+from Doraemon.settings import TASKS, KEYS
+from Doraemon.model import *
 
 if __name__ == '__main__':
-    # 初始化数据库表
+    print("初始化日志文件夹")
+    log_dir = "/var/log/Doraemon"
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    print("初始化系统配置")
     for key in KEYS:
+        item = System.objects.filter(key=key["key"])
+        if item.count() > 0:
+            print(f">{key['remark']} SKIPPED")
+            continue
         System.objects.create(**key)
+        print(f">{key['remark']} SUCCEED")
+
+    print("初始化推送消息")
+    for task in TASKS:
+        message = Message.objects.filter(task=task[0])
+        if message.count() > 0:
+            print(f">{task[1]} SKIPPED")
+            continue
+        Message.objects.create(task=task[0], remark=task[1])
+        print(f">{task[1]} SUCCEED")
+
+    print("所有初始化操作结束")
